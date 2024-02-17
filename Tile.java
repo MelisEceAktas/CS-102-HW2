75c2755210628f6637970168a06ee080ec36806c
Omer Can Baykara <omercanbaykara@gmail.com>
2024-02-17 15:35:26 +0300

    Implemented Tile class

--- Tile.java
+++ Tile.java
@@ -10,31 +10,42 @@ public class Tile {
     }
 
     /*
-     * TODO: should check if the given tile t and this tile have the same value 
+     * TODO (OMER): should check if the given tile t and this tile have the same value 
      * return true if they are matching, false otherwise
      */
     public boolean matchingTiles(Tile t) {
-        return false;
+        return value == t.value;
     }
 
     /*
-     * TODO: should compare the order of these two tiles
+     * TODO (OMER): should compare the order of these two tiles
      * return 1 if given tile has smaller in value
      * return 0 if they have the same value
      * return -1 if the given tile has higher value
      */
     public int compareTo(Tile t) {
-       return 0;
+        if (value > t.value)
+        {
+            return 1;
+        }
+        else if (value == t.value)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
     }
 
     /*
-     * TODO: should determine if this tile and given tile can form a chain together
+     * TODO (OMER): should determine if this tile and given tile can form a chain together
      * this method should check the difference in values of the two tiles
      * should return true if the absoulute value of the difference is 1 (they can form a chain)
      * otherwise, it should return false (they cannot form a chain)
      */
     public boolean canFormChainWith(Tile t) {
-        return false;
+        return (Math.abs(value - t.value) == 1);
     }
 
     public String toString() {
